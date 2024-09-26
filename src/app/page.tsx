2bfd6fa8c81c4913b5293721471b702d479e3113
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-09-26 23:04:35 +0900

    reacharts導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
 
   return (
     <main className='relative flex-1 before:content-[""] before:block before:h-full before:bg-[url("/assets/img/bg.png")]'>
-      <div className='absolute p-10 inset-0 bg-black bg-opacity-80'>
+      <div className='absolute p-10 inset-0 bg-black bg-opacity-80 grid grid-cols-subgrid'>
         <h2 className='flex gap-2 items-center border-b-[1px] border-white-20 pb-[16px] text-white font-normal text-[32px]'>
           <FontAwesomeIcon icon={faSquarePollVertical} color='white' className='w-[27px]' />
           取引価格 <span className='pl-[8px] text-[13.71px] h-[33px] flex items-end'>※取引面積1㎡あたり</span>
         </h2>
-        <div className='flex pt-10 pb-10 h-full'>
+        <div className='flex pt-10 gap-6'>
           <Graph />
           <Form setGraphData={setGraphData} />
         </div>
--- src/components/organisms/Graph/index.tsx
+++ src/components/organisms/Graph/index.tsx
@@ -1,35 +1,35 @@
-import React, { PureComponent } from 'react'
-import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import React from 'react'
+import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts'
 
 const Graph = () => {
   const data = [
-    {
-      name: 'Page A',
-      amt: 4000,
-    },
-    {
-      name: 'Page B',
-      amt: 2210,
-    },
+    { name: 'Page A', amt: 3361873 },
+    { name: '全国平均', amt: 1234567 },
   ]
+
   return (
-    <ResponsiveContainer width='100%' height='100%'>
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5,
-          right: 0,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <XAxis stroke='#fff' />
-        <YAxis stroke='#fff' label={{ value: 'YAxis Label' }} />
-        <Bar dataKey='amt' fill='#fff' activeBar={<Rectangle fill='pink' stroke='blue' />} />
-      </BarChart>
-    </ResponsiveContainer>
+    <div className='w-full flex gap-20 flex-col py-28 px-[372px]'>
+      <div className='w-full'>
+        <p>東京都</p>
+        <p>2022年</p>
+        <p>土地(住宅地)</p>
+      </div>
+      <ResponsiveContainer>
+        <BarChart data={data} barCategoryGap={20}>
+          <XAxis stroke='#fff' className='[&>g>g>line]:stroke-none' dataKey='name' />
+          <YAxis
+            width={100}
+            dx={-8}
+            stroke='#fff'
+            type='number'
+            domain={[0, 'auto']}
+            tickCount={14}
+            className='[&>g>g:last-child>text]:fill-transparent'
+          />
+          <Bar dataKey='amt' fill='#fff' barSize={200} />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
